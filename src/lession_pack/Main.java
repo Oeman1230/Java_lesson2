9e80d2a9786d98ca0825fbef21557e40800f4fd6
Oeman1230 <moskalev.iura@mail.ru>
2021-04-04 18:54:09 +0300

    Now i will work in main branch

--- src/lession_pack/Main.java
+++ src/lession_pack/Main.java
@@ -3,6 +3,10 @@ package lession_pack;
 public class Main {
 
     public static void main(String[] args) {
-	System.out.println("test");
+	StringBuffer name1 = new StringBuffer("kolya");
+	StringBuffer name2 = new StringBuffer("Pasha");
+	StringBuffer favFod = new StringBuffer("tres");
+	objGiraffe giraffe = new objGiraffe(name1, 100, 10, 14, favFod);
+	giraffe.girGiveName(name2);
     }
 }
