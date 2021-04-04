e9928b9ac6bb975f55ed25f9afd4568a4bdcd676
Oeman1230 <moskalev.iura@mail.ru>
2021-04-04 18:02:35 +0300

    separate objects in different files

---
+++ src/lession_pack/objRoom.java
@@ -0,0 +1,66 @@
+package lession_pack;
+
+public class objRoom
+{
+    objRoom(){
+        roomName = new StringBuffer();
+    }
+
+    StringBuffer roomName;
+    Integer roomArea;
+    Integer roomHeidth;
+    Integer roomWindowsCount;
+    Integer roomCreaturesCount;
+    void roomAddCreature(Integer creatureHeidth, Integer creatureSize, StringBuffer CreatureName)
+    {
+        if(creatureHeidth < roomHeidth && creatureSize < roomArea)
+        {
+            roomCreaturesCount ++;
+            System.out.println("В комнату пришел(а)");
+            System.out.println(CreatureName);
+        }
+    }
+    void roomAddWindow(Integer winCount){
+        roomWindowsCount += winCount;
+        System.out.println("добавлено" + winCount+ "окон" );
+    }
+    void roomAddArea(Integer addArea){
+        roomArea += addArea;
+        System.out.println("Добавлено" + addArea +"Квадратных метров");
+    }
+    void roomParty()
+    {
+        if(roomWindowsCount > 2)
+        {
+            System.out.println("Вечеринка началась с "+ roomWindowsCount+ "существами");
+        }
+        else
+        {
+            System.out.println("недостаточно существ для вечеринки");
+        }
+    }
+    void roomWentOut(StringBuffer CreatureName)
+    {
+        if(roomCreaturesCount >= 1)
+        {
+            System.out.println("Вышел из комнаты:");
+            System.out.println(CreatureName);
+            roomCreaturesCount --;
+        }
+        else
+        {
+            System.out.println("здесь никого нет");
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "objRoom{" +
+                "roomName=" + roomName +
+                ", roomArea=" + roomArea +
+                ", roomHeidth=" + roomHeidth +
+                ", roomWindowsCount=" + roomWindowsCount +
+                ", roomCreaturesCount=" + roomCreaturesCount +
+                '}';
+    }
+}
