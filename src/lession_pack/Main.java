ed16f4bc4e8ff2cffbe4c4ee4c539d02f9fd3d26
Oeman1230 <moskalev.iura@mail.ru>
2021-04-06 09:43:24 +0300

    add while(). Check methods eqals() and toString()

--- src/lession_pack/Main.java
+++ src/lession_pack/Main.java
@@ -1,12 +1,48 @@
 package lession_pack;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
 	StringBuffer name1 = new StringBuffer("kolya");
 	StringBuffer name2 = new StringBuffer("Pasha");
 	StringBuffer favFod = new StringBuffer("tres");
+	Scanner keyboard = new Scanner(System.in);
 	objGiraffe giraffe = new objGiraffe(name1, 100, 10, favFod);
+
+	StringBuffer lookGir = new StringBuffer("Посмотреть жирафа");
+	StringBuffer lookHum = new StringBuffer("Посмотреть человека");
+	StringBuffer lookRoom = new StringBuffer("Посмотреть комнату");
+	StringBuffer DoExit = new StringBuffer("Выход");
 	giraffe.girGiveName(name2);
+	StringBuffer keybuffer = new StringBuffer();
+	Integer i;
+	i = 0;
+		System.out.println("Кого вы хотите посмотреть?");
+		System.out.println(lookHum);
+		System.out.println(lookGir);
+		System.out.println(lookRoom);
+		System.out.println("Выход");
+		while(i != 1)
+	{
+
+		keybuffer.delete(0,keybuffer.length());
+		keybuffer.append(keyboard.next());
+		System.out.println(keybuffer);
+
+
+		if(keybuffer.toString().equals(lookGir.toString()))
+		{
+			System.out.println("giraffe action check");
+			continue;
+		}
+		if(keybuffer.toString().equals(DoExit.toString()))
+		{
+			i = 1;
+			break;
+		}
+		System.out.println("Неверная комманда");
+	}
     }
 }
